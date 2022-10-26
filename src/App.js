212029e08546ff9db01f51f809693cdd9106592e
Anupam Vamsi <anupam.ismav@gmail.com>
2022-10-26 23:11:04 +0530

    1. Convert App() to class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
