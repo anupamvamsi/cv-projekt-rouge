80a3cfe62dcb85d3cd827e318b4924fd35d51bc4
Anupam Vamsi <anupam.ismav@gmail.com>
2022-10-31 12:20:53 +0530

    1. Add individual states of value and checked depending on input[type] and an onChange handler

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,14 +4,43 @@ export class Input extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.onChange = this.onChange.bind(this);
+
+    this.state = {
+      value: '',
+      checked: false,
+    };
+  }
+
+  onChange(e) {
+    if (e.target.type === 'text' || e.target.type === 'date') {
+      this.setState(
+        {
+          value: e.target.value,
+        },
+        () => console.log(`value: ${this.state.value} `)
+      );
+    }
+
+    if (e.target.type === 'checkbox') {
+      this.setState(
+        {
+          checked: e.target.checked,
+        },
+        () => console.log(`check: ${this.state.checked}`)
+      );
+    }
   }
 
   render() {
     return (
       <label className="input-label">
         {this.props.labelDesc}
-        <input className={this.props.className} type={this.props.inputType} />
+        <input
+          className={this.props.className}
+          type={this.props.inputType}
+          onChange={this.onChange}
+        />
       </label>
     );
   }
