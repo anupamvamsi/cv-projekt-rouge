ab8685377a0d6303626c8248bdf6f3ce71598856
Anupam Vamsi <anupam.ismav@gmail.com>
2022-11-05 20:45:26 +0530

    1. Add delete functionality, 2. Add conditional rendering of add button when all items of a section are deleted, 3. Update css for btn-add, 4. Link css file in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './styles/sections.css';
 import React, { Component } from 'react';
 import uniqid from 'uniqid';
 import { Section } from './components/Section';
@@ -6,31 +7,52 @@ import { WorkSection } from './components/WorkSection';
 import { SectionTitle } from './components/SectionTitle';
 import { EducationSection } from './components/EducationSection';
 import { PersonalSection } from './components/PersonalSection';
+import { BtnAdd } from './components/BtnAdd';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+    this.addSection = this.addSection.bind(this);
+    this.delSection = this.delSection.bind(this);
+
+    const firstIDWork = uniqid();
+    const firstIDEdu = uniqid();
     this.state = {
       numWorkSection: 1,
       workSections: [
-        <WorkSection key={uniqid()} add={() => this.addSection('work')} />,
+        <WorkSection
+          key={firstIDWork}
+          add={() => this.addSection('work')}
+          del={(e) => this.delSection(e, firstIDWork, 'work')}
+        />,
       ],
 
       numEduSection: 1,
       eduSections: [
-        <EducationSection key={uniqid()} add={() => this.addSection('edu')} />,
+        <EducationSection
+          key={firstIDEdu}
+          add={() => this.addSection('edu')}
+          del={(e) => this.delSection(e, firstIDEdu, 'edu')}
+        />,
       ],
     };
   }
 
   addSection(sectionType) {
     if (sectionType === 'work') {
+      const idWork = uniqid();
       this.setState(
         {
           numWorkSection: this.state.numWorkSection + 1,
           workSections: this.state.workSections.concat(
-            <WorkSection key={uniqid()} add={() => this.addSection('work')} />
+            <WorkSection
+              key={idWork}
+              add={() => this.addSection('work')}
+              del={(e) => {
+                this.delSection(e, idWork, 'work');
+              }}
+            />
           ),
         },
         () => {
@@ -40,19 +62,49 @@ class App extends Component {
     }
 
     if (sectionType === 'edu') {
+      const idEdu = uniqid();
       this.setState(
         {
           numEduSection: this.state.numEduSection + 1,
           eduSections: this.state.eduSections.concat(
             <EducationSection
-              key={uniqid()}
+              key={idEdu}
               add={() => this.addSection('edu')}
+              del={(e) => {
+                this.delSection(e, idEdu, 'edu');
+              }}
             />
           ),
         },
         () => {
           console.log(this.state.numEduSection, this.state.eduSections);
         }
+      );
+    }
+  }
+
+  delSection(e, uniqueID, sectionType) {
+    console.log('uID: ', uniqueID);
+
+    if (sectionType === 'work') {
+      this.setState(
+        {
+          workSections: this.state.workSections.filter(
+            (s) => s.key !== uniqueID
+          ),
+          numWorkSection: this.state.numWorkSection - 1,
+        },
+        () => console.log(this.state.workSections)
+      );
+    }
+
+    if (sectionType === 'edu') {
+      this.setState(
+        {
+          eduSections: this.state.eduSections.filter((s) => s.key !== uniqueID),
+          numEduSection: this.state.numEduSection - 1,
+        },
+        () => console.log(this.state.eduSections)
       );
     }
   }
@@ -73,11 +125,34 @@ class App extends Component {
           </Section>
 
           <SectionTitle className="section-title">Work Experience</SectionTitle>
-          <Section className="info-work">{this.state.workSections}</Section>
+          <Section className="info-work">
+            {this.state.workSections}
+            {(() => {
+              if (this.state.workSections.length === 0) {
+                return (
+                  <BtnAdd
+                    className="btn-add"
+                    add={() => this.addSection('work')}
+                  />
+                );
+              }
+            })()}
+          </Section>
 
           <SectionTitle className="section-title">Education</SectionTitle>
           <Section className="info-education" sectionTitle="Education">
             {this.state.eduSections}
+
+            {(() => {
+              if (this.state.eduSections.length === 0) {
+                return (
+                  <BtnAdd
+                    className="btn-add"
+                    add={() => this.addSection('edu')}
+                  />
+                );
+              }
+            })()}
           </Section>
         </div>
       </div>
