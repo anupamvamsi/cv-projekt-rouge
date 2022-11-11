01f8b21ef14b98bd918cc81a623e2ea3f22f2d54
Anupam Vamsi <anupam.ismav@gmail.com>
2022-11-11 16:53:48 +0530

    1. Input - convert to functional (fnal) component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,27 +1,14 @@
-import React, { Component } from 'react';
-
-export class Input extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: '',
-      checked: false,
-    };
-  }
-
-  render() {
-    return (
-      <label className="input-label">
-        {this.props.labelDesc}
-        <input
-          className={this.props.className}
-          type={this.props.inputType}
-          value={this.props.value}
-          checked={this.props.checked}
-          onChange={this.props.handleChange}
-        />
-      </label>
-    );
-  }
+export function Input(props) {
+  return (
+    <label className="input-label">
+      {props.labelDesc}
+      <input
+        className={props.className}
+        type={props.inputType}
+        value={props.value}
+        checked={props.checked}
+        onChange={props.handleChange}
+      />
+    </label>
+  );
 }
