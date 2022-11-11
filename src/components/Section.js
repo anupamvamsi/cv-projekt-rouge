c2e22a07375f49253de356abff3222cae40fc151
Anupam Vamsi <anupam.ismav@gmail.com>
2022-11-11 15:11:25 +0530

    1. Section - convert to functional (fnal) component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,14 +1,3 @@
-import React, { Component } from 'react';
-
-export class Section extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <section className={this.props.className}>{this.props.children}</section>
-    );
-  }
+export function Section(props) {
+  return <section className={props.className}>{props.children}</section>;
 }
