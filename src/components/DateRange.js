9d01d0901234a1301082bc003c13b96a4ba2a6a6
Anupam Vamsi <anupam.ismav@gmail.com>
2022-11-11 16:30:48 +0530

    1. DateRange - convert to functional (fnal) component

--- src/components/DateRange.js
+++ src/components/DateRange.js
@@ -1,48 +1,39 @@
-import React, { Component } from 'react';
 import { Input } from './Input';
 
-export class DateRange extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="date-range">
-        <div className="input-dates">
-          <Input
-            className="input-date-start"
-            labelDesc="From"
-            inputType="date"
-            value={this.props.dateStart}
-            handleChange={this.props.handleChange}
-          />
-          {/* "OR" it with a "present" checkbox by using workingStatus */}
-          {(() => {
-            if (!this.props.currentlyWorking) {
-              return (
-                <Input
-                  className="input-date-end"
-                  labelDesc="To"
-                  inputType="date"
-                  value={this.props.dateEnd}
-                  handleChange={this.props.handleChange}
-                />
-              );
-            }
-          })()}
-        </div>
-
+export function DateRange(props) {
+  return (
+    <div className="date-range">
+      <div className="input-dates">
         <Input
-          className="input-checkbox"
-          labelDesc="Currently here"
-          inputType="checkbox"
-          checked={this.props.currentlyWorking}
-          handleChange={this.props.handleChange}
+          className="input-date-start"
+          labelDesc="From"
+          inputType="date"
+          value={props.dateStart}
+          handleChange={props.handleChange}
         />
+        {/* "OR" it with a "present" checkbox by using workingStatus */}
+        {(() => {
+          if (!props.currentlyWorking) {
+            return (
+              <Input
+                className="input-date-end"
+                labelDesc="To"
+                inputType="date"
+                value={props.dateEnd}
+                handleChange={props.handleChange}
+              />
+            );
+          }
+        })()}
       </div>
-    );
-  }
+
+      <Input
+        className="input-checkbox"
+        labelDesc="Currently here"
+        inputType="checkbox"
+        checked={props.currentlyWorking}
+        handleChange={props.handleChange}
+      />
+    </div>
+  );
 }
