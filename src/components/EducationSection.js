726727e7bfd80245ae2bb8ac80bd7e72bb282cb6
Anupam Vamsi <anupam.ismav@gmail.com>
2022-11-16 23:16:32 +0530

    1. EducationSection - convert to functional (fnal) component

--- src/components/EducationSection.js
+++ src/components/EducationSection.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Section } from './Section';
 import { Input } from './Input';
 import { DateRange } from './DateRange';
@@ -7,122 +7,93 @@ import { BtnSubmit } from './BtnSubmit';
 import { BtnAdd } from './BtnAdd';
 import { BtnEdit } from './BtnEdit';
 
-export class EducationSection extends Component {
-  constructor(props) {
-    super(props);
+export function EducationSection(props) {
+  const [school, setSchool] = useState('');
+  const [areaStudy, setAreaStudy] = useState('');
+  const [dateStart, setDateStart] = useState('');
+  const [dateEnd, setDateEnd] = useState('');
+  const [currentlyWorking, setCurrentlyWorking] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
-
-    this.state = {
-      school: '',
-      areaStudy: '',
-      dateStart: '',
-      dateEnd: '',
-      currentlyWorking: false,
-
-      submitted: false,
-    };
-  }
-
-  handleChange(e) {
+  const handleChange = (e) => {
     if (e.target.classList.contains('input-school')) {
-      this.setState({
-        school: e.target.value,
-      });
+      setSchool(e.target.value);
     } else if (e.target.classList.contains('input-study')) {
-      this.setState({
-        areaStudy: e.target.value,
-      });
+      setAreaStudy(e.target.value);
     } else if (e.target.classList.contains('input-date-start')) {
-      this.setState({
-        dateStart: e.target.value,
-      });
+      setDateStart(e.target.value);
     } else if (e.target.classList.contains('input-date-end')) {
-      this.setState((state, props) => {
-        if (
-          this.state.dateStart !== '' &&
-          this.state.currentlyWorking === false &&
-          this.state.dateStart < e.target.value
-        ) {
-          return {
-            dateEnd: e.target.value,
-          };
-        }
-      });
+      if (
+        dateStart !== '' &&
+        currentlyWorking === false &&
+        dateStart < e.target.value
+      ) {
+        setDateEnd(e.target.value);
+      }
     } else if (e.target.classList.contains('input-checkbox')) {
-      this.setState({
-        currentlyWorking: e.target.checked,
-      });
+      setCurrentlyWorking(e.target.checked);
     }
-  }
+  };
 
-  handleSubmit(e) {
-    this.setState({
-      submitted: true,
-    });
-  }
+  const handleSubmit = (e) => {
+    setSubmitted(true);
+  };
 
-  handleEdit(e) {
-    this.setState({
-      submitted: false,
-    });
-  }
+  const handleEdit = (e) => {
+    setSubmitted(false);
+  };
 
-  render() {
-    if (!this.state.submitted) {
-      return (
-        <Section className="details-education">
-          <Input
-            className="input-txt input-school"
-            labelDesc="School"
-            inputType="text"
-            value={this.state.school}
-            handleChange={this.handleChange}
-          />
-          <Input
-            className="input-txt input-study"
-            labelDesc="Area of study"
-            inputType="text"
-            value={this.state.areaStudy}
-            handleChange={this.handleChange}
-          />
-          <DateRange
-            dateStart={this.state.dateStart}
-            dateEnd={this.state.dateEnd}
-            currentlyWorking={this.state.currentlyWorking}
-            handleChange={this.handleChange}
-          />
+  if (!submitted) {
+    return (
+      <Section className="details-education">
+        <Input
+          className="input-txt input-school"
+          labelDesc="School"
+          inputType="text"
+          value={school}
+          handleChange={handleChange}
+        />
+        <Input
+          className="input-txt input-study"
+          labelDesc="Area of study"
+          inputType="text"
+          value={areaStudy}
+          handleChange={handleChange}
+        />
+        <DateRange
+          dateStart={dateStart}
+          dateEnd={dateEnd}
+          currentlyWorking={currentlyWorking}
+          handleChange={handleChange}
+        />
 
-          <div className="btn-container">
-            <BtnSubmit handleSubmit={this.handleSubmit} />
-            <BtnDelete del={this.props.del} />
-            <BtnAdd add={this.props.add} />
-          </div>
-        </Section>
-      );
-    } else {
-      return (
-        <Section className="details-education">
-          <p>{this.state.school}</p>
-          <p>{this.state.areaStudy}</p>
-          <p>
-            {(() => {
-              if (!this.state.currentlyWorking) {
-                return `${this.state.dateStart} to ${this.state.dateEnd}`;
-              } else {
-                return `${this.state.dateStart} to present`;
-              }
-            })()}
-          </p>
+        <div className="btn-container">
+          <BtnSubmit handleSubmit={handleSubmit} />
+          <BtnDelete del={props.del} />
+          <BtnAdd add={props.add} />
+        </div>
+      </Section>
+    );
+  } else {
+    return (
+      <Section className="details-education">
+        <p>{school}</p>
+        <p>{areaStudy}</p>
+        <p>
+          {(() => {
+            if (!currentlyWorking) {
+              return `${dateStart} to ${dateEnd}`;
+            } else {
+              return `${dateStart} to present`;
+            }
+          })()}
+        </p>
 
-          <div className="btn-container">
-            <BtnEdit handleEdit={this.handleEdit} />
-            <BtnAdd add={this.props.add} />
-          </div>
-        </Section>
-      );
-    }
+        <div className="btn-container">
+          <BtnEdit handleEdit={handleEdit} />
+          <BtnAdd add={props.add} />
+        </div>
+      </Section>
+    );
   }
 }
